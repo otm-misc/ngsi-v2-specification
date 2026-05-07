818f5179658e91be10e12cf9c5c8c6be833e1a0c
MIKAMI <masaru.mikami@opentone.co.jp>
2026-05-07 12:35:30 +0900

    feat: 参照先URL を NGSI v2 の API仕様に差し替え

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -3,17 +3,17 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "ngsiv2-openapi.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
-    presets: [
-      SwaggerUIBundle.presets.apis,
-      SwaggerUIStandalonePreset
-    ],
+    // presets: [
+    //   SwaggerUIBundle.presets.apis,
+    //   // SwaggerUIStandalonePreset
+    // ],
     plugins: [
       SwaggerUIBundle.plugins.DownloadUrl
     ],
-    layout: "StandaloneLayout"
+    // layout: "StandaloneLayout"
   });
 
   //</editor-fold>
